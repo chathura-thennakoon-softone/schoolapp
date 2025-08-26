58e156c0b4a64202772e02651cf77c59964da73b
chathura thennakoon <chathura.thennakoon@softone.lk>
2025-08-26 22:30:00 +0530

    add StudentCourse

---
+++ SCH/SCH.Client/src/app/sch/student/selectors/student-course/student-course.ts
@@ -0,0 +1,217 @@
+import { ChangeDetectorRef, Component, Inject, input } from '@angular/core';
+import { StudentCourseMap } from '../../../../sch/interfaces/student-course-map';
+import {
+  AllCommunityModule,
+  CellClickedEvent,
+  ColDef,
+  ModuleRegistry,
+} from 'ag-grid-community';
+import { StudentApi } from '../../services/student-api';
+import { AgGridAngular } from 'ag-grid-angular';
+import { Course } from '../../../../sch/interfaces/course';
+import { CourseApi } from '../../../../sch/services/course-api';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { concat, forkJoin } from 'rxjs';
+import { ConfirmDialog } from '../../../../selectors/confirm-dialog/confirm-dialog';
+import { MatDialog } from '@angular/material/dialog';
+import { Notification } from '../../../../services/notification';
+
+ModuleRegistry.registerModules([AllCommunityModule]);
+
+@Component({
+  selector: 'sch-student-course',
+  imports: [CommonModule, FormsModule, AgGridAngular],
+  templateUrl: './student-course.html',
+  styleUrl: './student-course.scss'
+})
+export class StudentCourse {
+  studentId = input.required<number>();
+
+  protected readonly columnDefs: ColDef<
+    StudentCourseMap,
+    number | string | Date | boolean | null
+  >[] = [
+    {
+      headerName: 'StudentId',
+      field: 'studentId',
+      sortable: true,
+      filter: true,
+    },
+    {
+      headerName: 'CourseId',
+      field: 'courseId',
+      sortable: true,
+      filter: true,
+    },
+    {
+      headerName: 'Enrollment Date',
+      field: 'enrollmentDate',
+      sortable: true,
+      filter: true,
+    },
+    {
+      headerName: 'Student First Name',
+      field: 'studentFirstName',
+      sortable: true,
+      filter: true,
+    },
+    {
+      headerName: 'Student Last Name',
+      field: 'studentLastName',
+      sortable: true,
+      filter: true,
+    },
+    {
+      headerName: 'Course Name',
+      field: 'courseName',
+      sortable: true,
+      filter: true,
+    },
+    {
+      headerName: 'Actions',
+      cellRenderer: (params: any) => {
+        return `
+      <button type="button" class="delete-btn" data-action="delete">Delete</button>
+    `;
+      },
+      width: 200,
+      suppressMovable: true,
+    },
+  ];
+
+  protected rowData: StudentCourseMap[] = [];
+  private courses: Course[] = [];
+  protected notSelectedCourses: Course[] = [];
+
+  protected selectedCourseId: string | null = null;
+
+  protected dataLoading = false;
+  protected gridDataLoading = false;
+  protected isDeleting = false;
+  protected isAdding = false;
+  protected isEditing = false;
+
+  constructor(
+    private readonly cdr: ChangeDetectorRef,
+    private readonly studentApi: StudentApi,
+    private readonly courseApi: CourseApi,
+    @Inject(MatDialog) private readonly dialog: MatDialog,
+    private readonly notification: Notification
+  ) {}
+
+  ngOnInit(): void {
+    this.setData();
+  }
+
+  private setData(): void {
+    this.dataLoading = true;
+    this.cdr.markForCheck();
+  const courses$ = this.courseApi.getCourses();
+  const studentCourses$ = this.studentApi.getCourses(this.studentId());
+
+    forkJoin([courses$, studentCourses$]).subscribe(
+      ([courses, studentCourses]) => {
+        this.courses = courses;
+        this.rowData = studentCourses;
+        this.setNotSelectedCourses();
+        this.dataLoading = false;
+        this.cdr.markForCheck();
+      }
+    );
+  }
+
+  private resetGridData(): void {
+    this.gridDataLoading = true;
+    this.cdr.markForCheck();
+  this.studentApi.getCourses(this.studentId()).subscribe((courses) => {
+      this.rowData = courses;
+      this.setNotSelectedCourses();
+      this.gridDataLoading = false;
+      this.cdr.markForCheck();
+    });
+  }
+
+  protected onCellClicked(event: CellClickedEvent): void {
+    if (event.colDef.headerName === 'Actions') {
+      const target = event.event!.target as HTMLElement;
+      if (target.dataset['action'] === 'delete') {
+        this.onDeletes([event.data]);
+      }
+    }
+  }
+
+  protected onAddCourse(): void {
+    const courseId = Number(this.selectedCourseId);
+    if (courseId) {
+      const course: StudentCourseMap = {
+        studentId: this.studentId(),
+        courseId: courseId,
+        enrollmentDate: new Date(),
+        studentFirstName: null,
+        studentLastName: null,
+        courseName: null,
+      };
+
+      this.isAdding = true;
+      this.cdr.markForCheck();
+      this.studentApi
+        .insertCourse(this.studentId(), courseId, course)
+        .subscribe(() => {
+          this.resetGridData();
+          this.selectedCourseId = null;
+          this.notification.success('Course added successfully');
+        })
+        .add(() => {
+          this.isAdding = false;
+          this.cdr.markForCheck();
+        });
+    }
+  }
+
+  private setNotSelectedCourses(): void {
+    this.notSelectedCourses = this.courses.filter(
+      (course) =>
+        !this.rowData.some(
+          (studentCourse) => studentCourse.courseId === course.id
+        )
+    );
+  }
+
+  protected onRemoveAll(): void {
+    const dialogRef = this.dialog.open(ConfirmDialog, {
+      data: {
+        message: 'Are you sure you want to remove all courses?',
+        cancelText: 'Cancel',
+        confirmText: 'Delete',
+      },
+    });
+    dialogRef.afterClosed().subscribe((result) => {
+      if (result) {
+        this.onDeletes(this.rowData);
+      }
+    });
+  }
+
+  protected onDeletes(courses: StudentCourseMap[]): void {
+    const deleteRequests = courses.map((course) =>
+      this.studentApi.deleteCourse(this.studentId(), course.courseId)
+    );
+    this.isDeleting = true;
+    this.cdr.markForCheck();
+    concat(...deleteRequests)
+      .subscribe({
+        complete: () => {
+          this.resetGridData();
+          this.notification.success('Course deleted successfully');
+        },
+        error: (err) => {
+          this.notification.error('Failed to delete course');
+        },
+      })
+      .add(() => {
+        this.isDeleting = false;
+        this.cdr.markForCheck();
+      });
+  }
+}
